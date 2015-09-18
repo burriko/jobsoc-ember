8375209d1c6cfb549cff3c13786fdc3024bf21a6
Graeme Tait <burriko@gmail.com>
2015-09-18 22:05:01 +0100

    Function to display full name

--- app/controllers/student/index.js
+++ app/controllers/student/index.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+    fullName: function() {
+        return this.get('model.title') + ' ' + this.get('model.first_name') + ' ' + this.get('model.surname');
+    }.property('model.title', 'model.firstName', 'model.surname'),
+
     activePlacements: function() {
         return this.get('model.placements').filterBy('active');
     }.property('model.placements.@each.active'),
